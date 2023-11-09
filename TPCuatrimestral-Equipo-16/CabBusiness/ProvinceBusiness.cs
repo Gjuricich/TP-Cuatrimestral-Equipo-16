aa275e76ba484a11cf4df4b9acea8684fc13cfae
Gjuricich <132496477+Gjuricich@users.noreply.github.com>
2023-11-09 11:47:57 -0300

    Clase Provincia,ProvinciaNegocio,Método listar de DB

---
+++ TPCuatrimestral-Equipo-16/CabBusiness/ProvinceBusiness.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabDominio;
+
+namespace CabBusiness
+{
+    public class ProvinceBusiness
+    {
+
+        public List<Province>List()
+        {
+            List<Province> list = new List<Province>();
+            DataManager dataManager = new DataManager();
+
+            try
+            {
+
+                dataManager.setQuery("SELECT IdProvincia, NombreProvincia,Estado FROM PROVINCIAS");
+                dataManager.executeRead();
+                while (dataManager.Lector.Read())
+                {
+                    Province province = new Province();
+                    province.IdProvince = (int)(long)dataManager.Lector["IdProvincia"];
+                    province.NameProvince = (string)dataManager.Lector["NombreProvincia"];
+                    province.State = (bool)dataManager.Lector["Estado"];
+
+                    list.Add(province);
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                dataManager.closeConection();
+            }
+        }
+    }
+}
---
+++ TPCuatrimestral-Equipo-16/CabDominio/Province.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabDominio
+{
+    public class Province
+    {  
+        public int IdProvince { get; set; }
+        public string NameProvince { get; set; }
+        public bool State { get; set; }
+    }
+}
+
