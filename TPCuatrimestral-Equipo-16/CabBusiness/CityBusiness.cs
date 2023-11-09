966b47002c6781c334c6a4dd952a587b905d30bf
Gjuricich <132496477+Gjuricich@users.noreply.github.com>
2023-11-09 12:03:13 -0300

    Clase Ciudad, CiudadNegocio,Método listar desde DB

---
+++ TPCuatrimestral-Equipo-16/CabBusiness/CityBusiness.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabDominio;
+
+namespace CabBusiness
+{
+    public class CityBusiness
+    {
+        public List<City> List()
+        {
+            List<City> list = new List<City>();
+            DataManager dataManager = new DataManager();
+
+            try
+            {
+
+                dataManager.setQuery("SELECT IdCiudad,IdProvincia, NombreCiudad,Estado FROM CIUDADES");
+                dataManager.executeRead();
+                while (dataManager.Lector.Read())
+                {
+                    City city = new City();
+                    city.IdCity = (int)(long)dataManager.Lector["IdCiudad"];
+                    city.IdProvince = (int)(long)dataManager.Lector["IdProvincia"];
+                    city.NameCity = (string)dataManager.Lector["NombreCiudad"];
+                    city.State = (bool)dataManager.Lector["Estado"];
+
+                    list.Add(city);
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                dataManager.closeConection();
+            }
+        }
+}
---
+++ TPCuatrimestral-Equipo-16/CabDominio/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabDominio
+{
+    public class City
+    {
+        public int IdCity { get; set; }
+        public int IdProvince { get; set; }
+        public string NameCity { get; set; }
+        public bool State { get; set; }
+
+    }
+}
